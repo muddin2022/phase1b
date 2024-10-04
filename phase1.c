60161b3c1a46d2311b5c6aa28fe4f5914a78bf39
Corey Ramirez <159506030+unrevealer@users.noreply.github.com>
2024-10-03 19:50:51 -0700

    remove runqueue struct and

--- phase1.c
+++ phase1.c
@@ -28,24 +28,6 @@ struct PCB
     struct PCB *nextSibling;
 };
 
-struct RunQueue
-{
-    struct PCB *p1head;
-    struct PCB *p1tail;
-
-    struct PCB *p2head;
-    struct PCB *p2tail;
-
-    struct PCB *p3head;
-    struct PCB *p3tail;
-
-    struct PCB *p4head;
-    struct PCB *p4tail;
-
-    struct PCB *p5head;
-    struct PCB *p5tail;
-}
-
 /* ----------------------- Global variables ---------------------- */
 struct PCB runQueue;
 struct PCB *currProc;
